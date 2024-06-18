846c8122974292cceaeb6c03acd922c8f67b47d5
ayoub.abarji.pro@gmail.com <ayoub.abarji.pro@gmail.com>
2024-06-18 13:06:15 +0200

    Initial Commit

---
+++ Correction Text Extended/Correction_Text_ExtendedApp.swift
@@ -0,0 +1,17 @@
+//
+//  Correction_Text_ExtendedApp.swift
+//  Correction Text Extended
+//
+//  Created by Ayoub ABARJI on 17/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct Correction_Text_ExtendedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
